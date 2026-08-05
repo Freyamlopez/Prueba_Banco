1fa6e23e58e7efa2cac56d43ff1f80f4c9f5a19f
PRE OSTAIZA MERCHAN RANIA IG <rostaiza@sasf.local>
2026-08-05 11:08:35 -0500

    first commit

---
+++ sistemaFacturacion/src/test/java/prueba/sistemaFacturacion/SistemaFacturacionApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba.sistemaFacturacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaFacturacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
