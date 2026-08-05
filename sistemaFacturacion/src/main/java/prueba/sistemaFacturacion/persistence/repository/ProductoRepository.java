7d30b611e0ff80ecd72e0e586b6534f2a4ec8ecc
Freya Lopez L <131899019+Freyamlopez@users.noreply.github.com>
2026-08-05 14:30:10 -0500

    Revert "feat: se agrego el producto y serivce entity mas las clases de exceptiones globales"

--- sistemaFacturacion/src/main/java/prueba/sistemaFacturacion/persistence/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package prueba.sistemaFacturacion.persistence.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import prueba.sistemaFacturacion.persistence.entity.Producto;
-
-public interface ProductoRepository extends JpaRepository <Producto, Long> {
-}
--- sistemaFacturacion/src/main/java/prueba/sistemaFacturacion/web/excepcion/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package prueba.sistemaFacturacion.web.excepcion;
-
-
-public class ResourceNotFoundException extends  RuntimeException{
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
