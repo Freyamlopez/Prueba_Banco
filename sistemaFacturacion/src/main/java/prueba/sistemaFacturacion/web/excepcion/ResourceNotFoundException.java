ea3f835ea875db005bece8355e95fa351cdb120e
PRE HURTADO RODRIGUEZ JORDY <jhurtado@sasf.local>
2026-08-05 12:18:48 -0500

    feat: agrego producto entity mas las exceptiones globales

---
+++ sistemaFacturacion/src/main/java/prueba/sistemaFacturacion/web/excepcion/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package prueba.sistemaFacturacion.web.excepcion;
+
+
+public class ResourceNotFoundException extends  RuntimeException{
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
