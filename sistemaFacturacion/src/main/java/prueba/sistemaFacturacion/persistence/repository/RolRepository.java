de4bcc551e5dac801ac8d6c67b64e9b34c137192
PRE OSTAIZA MERCHAN RANIA IG <rostaiza@sasf.local>
2026-08-07 09:30:47 -0500

    Feat: agregro repositorio

---
+++ sistemaFacturacion/src/main/java/prueba/sistemaFacturacion/persistence/repository/RolRepository.java
@@ -0,0 +1,10 @@
+package prueba.sistemaFacturacion.persistence.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import prueba.sistemaFacturacion.persistence.entity.Rol;
+import java.util.Optional;
+
+
+public interface RolRepository extends JpaRepository<Rol, Long> {
+    Optional<Rol> findByNombre(String nombre);
+}
