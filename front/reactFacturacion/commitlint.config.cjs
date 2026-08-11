9b733bcc2011ec99fe1638587eac8b71e9d6fcbb
flopez <freya.lopez@sasf.net>
2026-08-11 14:10:56 -0500

    feat: reorganiza el repo en carpetas front y back, agrega gitignore y husky

---
+++ front/reactFacturacion/commitlint.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(\w+)(?:\(([^)]+)\))?: (.+)$/,
+      headerCorrespondence: ['type', 'scope', 'subject'],
+    },
+  },
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix']],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'header-max-length': [2, 'always', 100],
+  },
+};
